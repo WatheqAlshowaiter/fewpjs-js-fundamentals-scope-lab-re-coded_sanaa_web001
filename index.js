9952fb0c8f6de91ca16d856f2c8d15f897c830ea
Watheq Alshowaiter <watheq.alshowaiter@gmail.com>
2020-07-29 23:55:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const animal = "dog"
+const animal = "cat"
 
 function myAnimal() {
   return animal
@@ -8,6 +8,7 @@ function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
+  
   return animal
 }
 
